37e6972e98ba6f88961f29c38cd045255e4f9e24
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2021-06-21 16:54:45 +0530

    Update Count Complete Tree Nodes.cpp

--- TREE/BINARY TREE AND BST/Count Complete Tree Nodes.cpp
+++ TREE/BINARY TREE AND BST/Count Complete Tree Nodes.cpp
@@ -8,3 +8,27 @@ public:
             return (1+countNodes(root->left)+countNodes(root->right));
     }
 };
+
+
+OPTIMISE-
+    
+    class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        int rh=0,lh=0;
+        TreeNode*curr=root;
+        while (curr!=NULL) {
+            lh++;
+            curr=curr->left;
+        }
+        curr=root;
+        while (curr!=NULL) {
+            rh++;
+            curr=curr->right;
+        }
+        if (lh==rh)
+            return (pow(2,lh)-1);
+        else 
+            return (1+countNodes(root->left)+countNodes(root->right));
+    }
+};
