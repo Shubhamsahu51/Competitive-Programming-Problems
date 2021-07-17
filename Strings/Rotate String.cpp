2fcd69d7f5351b09a02a55ab44dc715dedd2a37b
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2021-07-18 00:12:32 +0530

    Update and rename Rotate String to Rotate String.cpp

--- Strings/Rotate String.cpp
+++ Strings/Rotate String.cpp
@@ -29,3 +29,25 @@ public:
 
     }
 };
+
+ii)
+    
+    class Solution {
+public:
+    bool rotateString(string A, string B) 
+    {
+        if(A.size() != B.size())
+            return false;
+        if(A == B)      //compare strings
+            return true;
+        
+        int len = B.size();
+        while(len--)
+        {
+            if(A == B)
+                return true;
+            A = A.substr(1) + A[0];     // one shift on A
+        }
+        return false;
+    }
+};
