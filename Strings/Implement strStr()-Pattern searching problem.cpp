8ecd7d8312ff87a04139a65699112c272f315e3e
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2021-07-19 01:29:14 +0530

    Create Implement strStr()-Pattern searching problem.cpp

---
+++ Strings/Implement strStr()-Pattern searching problem.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int strStr(string txt, string pat) {
+        if (txt==pat)
+            return 0;
+        if (txt.length()<pat.length())
+            return -1;
+        int n=txt.length();
+        int m=pat.length();
+        int i,j;
+        for (i=0;i<=n-m;i++) {
+            for (j=0;j<m;j++) 
+                if (txt[i+j]!=pat[j])
+                    break;
+            if (j==m) {
+                return i;
+                break;
+            }
+        }
+     return -1;   
+    }
+};
