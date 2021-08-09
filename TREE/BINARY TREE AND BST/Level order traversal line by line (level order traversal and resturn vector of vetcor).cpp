b4820c36010ad397ffb55adabd3c31d35d113b57
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2021-08-09 18:07:28 +0530

    Update Level order traversal line by line (level order traversal and resturn vector of vetcor).cpp

--- TREE/BINARY TREE AND BST/Level order traversal line by line (level order traversal and resturn vector of vetcor).cpp
+++ TREE/BINARY TREE AND BST/Level order traversal line by line (level order traversal and resturn vector of vetcor).cpp
@@ -1,3 +1,50 @@
+i) BF-O(N)
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    //Node at distance k
+    void helper(TreeNode*root,int k,vector<int>&temp) {
+    if (!root)           //if k is more then height
+       return;
+    if (k==0)
+      temp.push_back(root->val);
+    else {
+        helper(root->left,k-1,temp);
+        helper(root->right,k-1,temp);
+    }
+}
+    
+    //calculate height
+    int height(TreeNode*root) {
+        if (!root)
+            return 0;
+        else {
+            return 1+max(height(root->left),height(root->right));
+        }
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        int ht=height(root);
+        for (int i=0;i<ht;i++) {
+            vector<int>temp;
+            helper(root,i,temp);
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
+
+ii)optimise-O(N)
  class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
