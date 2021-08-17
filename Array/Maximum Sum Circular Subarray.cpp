e1a66a1d232d0e434d0b0738a2d3d8dbef8f79e9
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2021-08-17 20:10:40 +0530

    Update Maximum Sum Circular Subarray.cpp

--- Array/Maximum Sum Circular Subarray.cpp
+++ Array/Maximum Sum Circular Subarray.cpp
@@ -31,7 +31,26 @@ Note:
 -30000 <= A[i] <= 30000
 1 <= A.length <= 30000
 ************************************************************************Solution*********************************************************************
+i)BF-
+ int maxcircular(int arr[],int n) {
+    int res=arr[0];
+    for (int i=0;i<n;i++) {
+        int curr_max=arr[i];
+        int curr_sum=arr[i];
+        for (int j=1;j<n;j++) {
+            int index=(i+j)%n;
+            curr_sum+=arr[index];
+            curr_max=max(curr_max,curr_sum);
+            
+        }
+        res=max(res,curr_max);
+    }
+    return res;
+}
 
+
+
+ii)optimise-
 class Solution {
 public:
     int normalmaxsum(vector<int>& A,int n) {                             //cal to noraml subarray sum
