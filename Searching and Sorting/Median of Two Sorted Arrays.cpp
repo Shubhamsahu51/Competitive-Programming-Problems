1d478d71177f8f2d43e38d4fd8d0f83a0607cfe9
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2021-08-25 12:38:29 +0530

    Update Median of Two Sorted Arrays.cpp

--- Searching and Sorting/Median of Two Sorted Arrays.cpp
+++ Searching and Sorting/Median of Two Sorted Arrays.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
-        if(nums1.size() > nums2.size())                         //to make nums1 smaller
+        if(nums1.size() > nums2.size())                         //to make nums1 smaller and before initialising check it
             nums1.swap(nums2);
         
         int n1 = nums1.size(), n2 = nums2.size();
@@ -35,4 +35,39 @@ public:
         }
         return 0;
     }
+};
+***********************************************gfg solution************************************
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+        if (nums2.size()<nums1.size()) {
+            nums1.swap(nums2);
+        }
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int begin1=0,end1=n1;                        //observe
+        while (begin1 <= end1) {
+            int i1=(begin1+end1)/2;
+            int i2=(n1+n2+1)/2-i1;
+            int min1=(i1==n1?INT_MAX:nums1[i1]);
+            int max1=(i1==0?INT_MIN:nums1[i1-1]);
+            int min2=(i2==n2?INT_MAX:nums2[i2]);
+            int max2=(i2==0?INT_MIN:nums2[i2-1]);
+            if (max1<=min2 and max2<=min1) {
+                if ((n1+n2)%2==0)
+                    return ((double)(max(max1,max2)+min(min1,min2))/2);
+                else 
+                    return ((double)max(max1,max2));
+            }
+            else if (max1 > min2)
+                end1=i1-1;
+            else
+                begin1=i1+1;
+            
+        }
+        return 0;
+        
+    }
+
 };
