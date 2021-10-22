ab35055e6330b689591e024cf9e8efbb3ed121aa
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2021-10-22 17:25:43 +0530

    Update Rotate90bothway.cpp

--- Matrix/Rotate90bothway.cpp
+++ Matrix/Rotate90bothway.cpp
@@ -1,11 +1,11 @@
 Anticlokwise=left rotate
 void rotateby90(vector<vector<int> >& matrix, int n) 
     { 
-       for (int i=0;i<n;i++) {
+       for (int i=0;i<n;i++) {           //swap elemets
            for (int j=i+1;j<n;j++)
                 swap(matrix[i][j],matrix[j][i]);
        } 
-       for (int i=0;i<n;i++) {
+       for (int i=0;i<n;i++) {     //reverse coloum wise
            int low=0,high=n-1;
            while (low < high) {
                 swap(matrix[low][i],matrix[high][i]);
@@ -19,11 +19,11 @@ Clockwise=rightrotate
 
 void rotateby90(vector<vector<int> >& matrix, int n) 
     { 
-       for (int i=0;i<n;i++) {
+       for (int i=0;i<n;i++) {                //swap elements
            for (int j=i+1;j<n;j++)
                 swap(matrix[i][j],matrix[j][i]);
        } 
-       for (int i=0;i<n;i++) {
+       for (int i=0;i<n;i++) {          //reverse row wise
            int low=0,high=n-1;
            while (low < high) {
                 swap(matrix[i][low],matrix[i][high]);
