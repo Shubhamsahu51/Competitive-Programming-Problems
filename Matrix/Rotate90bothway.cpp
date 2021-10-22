49e0905f4e5545009a71a8e3cd0f8be58878ccf2
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2021-10-22 17:24:04 +0530

    Create Rotate90bothway.cpp

---
+++ Matrix/Rotate90bothway.cpp
@@ -0,0 +1,34 @@
+Anticlokwise=left rotate
+void rotateby90(vector<vector<int> >& matrix, int n) 
+    { 
+       for (int i=0;i<n;i++) {
+           for (int j=i+1;j<n;j++)
+                swap(matrix[i][j],matrix[j][i]);
+       } 
+       for (int i=0;i<n;i++) {
+           int low=0,high=n-1;
+           while (low < high) {
+                swap(matrix[low][i],matrix[high][i]);
+                low++;
+                high--;
+           }
+       }
+    }
+
+Clockwise=rightrotate
+
+void rotateby90(vector<vector<int> >& matrix, int n) 
+    { 
+       for (int i=0;i<n;i++) {
+           for (int j=i+1;j<n;j++)
+                swap(matrix[i][j],matrix[j][i]);
+       } 
+       for (int i=0;i<n;i++) {
+           int low=0,high=n-1;
+           while (low < high) {
+                swap(matrix[i][low],matrix[i][high]);
+                low++;
+                high--;
+           }
+       }
+    }
