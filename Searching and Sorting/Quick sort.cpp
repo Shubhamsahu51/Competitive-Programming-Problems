edae78c371aa30bcb5cb91716cff76256e7a930f
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2021-10-22 13:38:07 +0530

    Update Quick sort.cpp

--- Searching and Sorting/Quick sort.cpp
+++ Searching and Sorting/Quick sort.cpp
@@ -1,36 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int partition (int A[],int l,int h) {
-    int pivot = A[l];
-    int i =l,j=h;
-    do {
-        do {i++;}while(A[i]<=pivot);
-        do {j--;}while (A[j]>pivot);
-        if (i < j) {
-            swap (A[i],A[j]);
-        }
-    }while (i < j);
     
-    swap(A[l],A[j]);
-    return j;
-}
-void quicksort (int A[],int l,int h) {
-    //base case
-    if (l >= h)
-        return;
-    //Recursive call
-    int p = partition (A,l,h);
-    //apply quick sort on left side 
-    quicksort (A,l,p);
-    //apply quick sort on right side
-    quicksort (A,p+1,h);
-}
+    int partition (int arr[], int low, int high)
+    {
+        int pivot=arr[high]; //take end elemenet as pivot element
+        int i=low;
+        for (int j=low;j<=high-1;j++) {
+            if (arr[j] < pivot) {
+                swap(arr[i],arr[j]);
+                i++;
+            }
+        }
+        swap(arr[i],arr[high]);       //got pivot element right position
+        return i;
+    }
+
+    void quickSort(int arr[], int low, int high)
+    {
+        if (low >= high)
+            return;
+        int p = partition (arr,low,high);
+        quickSort(arr,low,p-1);
+        quickSort(arr,p+1,high);
+    }
 
 int main () {
     int A[] = {50,70,60,90,40,80,10,20,30};
     int n = sizeof(A)/sizeof(int);
-    quicksort (A,0,n);
+    quicksort (A,0,n-1);
     for (int i=0;i<n;i++)
         cout << A[i] <<" ";
     
