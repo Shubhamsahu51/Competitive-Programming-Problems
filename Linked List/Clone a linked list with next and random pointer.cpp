396f9de6a386bcc1d564b033a2d8c6c6fd5f878d
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2021-11-11 14:56:14 +0530

    Update Clone a linked list with next and random pointer.cpp

--- Linked List/Clone a linked list with next and random pointer.cpp
+++ Linked List/Clone a linked list with next and random pointer.cpp
@@ -22,3 +22,26 @@ public:
 
 ii)make changes in original LL-
   
+Node *copyList(Node *head)
+    {
+        //insert clone LL node and connect next pointer of it
+        for (Node *curr=head;curr != NULL;) {
+            Node *Next = curr->next;
+            curr->next = new Node(curr->data);
+            curr->next->next=Next;
+            curr=Next;
+        }
+        //connect random of clone using random of original
+        for (Node *curr=head;curr != NULL;curr=curr->next->next) {
+            curr->next->arb = (curr->arb != NULL) ? (curr->arb->next) : (NULL);
+        }
+        //segregate both LL
+        Node *original=head,*copy=head->next,*temp=copy;
+        while (original != NULL and copy != NULL) {
+            original->next = (original->next != NULL)?(original->next->next):(original->next);
+            copy->next = (copy->next != NULL)?(copy->next->next):(copy->next);
+            original =original->next;
+            copy =copy->next;
+        }
+        return temp;
+    }
