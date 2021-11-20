7d8f69ec10e7d6cfea302c47ef55f06e082e32ed
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2021-11-21 02:27:07 +0530

    Update Adjaceny list to adjacency matrix.cpp

--- Graph/Adjaceny list to adjacency matrix.cpp
+++ Graph/Adjaceny list to adjacency matrix.cpp
@@ -1,124 +1,32 @@
-// C++ Program to implement
-// the above approach
 #include <bits/stdc++.h>
 using namespace std;
- 
-// Function to insert vertices to adjacency list
-void insert(vector<int> adj[], int u, int v)
-{
-    // Insert a vertex v to vertex u
-    adj[u].push_back(v);
-    return;
-}
- 
-// Function to display adjacency list
-void printList(vector<int> adj[], int V)
-{
-    for (int i = 0; i < V; i++) {
-        cout << i;
-        for (auto j : adj[i])
-            cout << " --> " << j;
-        cout << endl;
-    }
-    cout << endl;
-}
- 
-// Function to convert adjacency
-// list to adjacency matrix
-vector<vector<int> > convert(vector<int> adj[],
-                             int V)
-{
-    // Initialize a matrix
-    vector<vector<int> > matrix(V,
-                                vector<int>(V, 0));
- 
-    for (int i = 0; i < V; i++) {
-        for (auto j : adj[i])
-            matrix[i][j] = 1;
-    }
-    return matrix;
-}
- 
-// Function to display adjacency matrix
-void printMatrix(vector<vector<int> > adj, int V)
-{
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
-            cout << adj[i][j] << "   ";
+void convert(vector<vector<int>>&a) {
+    vector<vector<int>>adj(a.size());
+    
+    for (int i=0;i<a.size();i++) {
+        for (int j=0;j<a[i].size();j++) {
+            if (a[i][j]==1)
+                adj[i].push_back(j);
         }
-        cout << endl;
     }
-    cout << endl;
+    for (int i=0;i<a.size();i++) {
+        for (int x:adj[i])
+            cout<<x<<" ";
+        cout<<endl;
+    }
 }
- 
-// Driver code
+
 int main()
 {
-    int V = 5;
- 
-    vector<int> adjList[V];
- 
-    // Inserting edges
-    insert(adjList, 0, 1);
-    insert(adjList, 0, 4);
-    insert(adjList, 1, 0);
-    insert(adjList, 1, 2);
-    insert(adjList, 1, 3);
-    insert(adjList, 1, 4);
-    insert(adjList, 2, 1);
-    insert(adjList, 2, 3);
-    insert(adjList, 3, 1);
-    insert(adjList, 3, 2);
-    insert(adjList, 3, 4);
-    insert(adjList, 4, 0);
-    insert(adjList, 4, 1);
-    insert(adjList, 4, 3);
- 
-    // Display adjacency list
-    cout << "Adjacency List: \n";
-    printList(adjList, V);
- 
-    // Function call which returns
-    // adjacency matrix after conversion
-    vector<vector<int> > adjMatrix
-        = convert(adjList, V);
- 
-    // Display adjacency matrix
-    cout << "Adjacency Matrix: \n";
-    printMatrix(adjMatrix, V);
- 
+    vector<vector<int>> a;
+    vector<int> p({0, 0, 1});
+    vector<int> q({0, 0, 1});
+    vector<int> r({1, 1, 0}); // adjacency matrix
+    a.push_back(p);
+    a.push_back(q);
+    a.push_back(r);
+    
+   
+    convert(a);
     return 0;
 }
-
-
-Adjacency List: 
-
-0 --> 1 --> 4
-
-1 --> 0 --> 2 --> 3 --> 4
-
-2 --> 1 --> 3
-
-3 --> 1 --> 2 --> 4
-
-4 --> 0 --> 1 --> 3
-
-
-
-Adjacency Matrix: 
-
-0   1   0   0   1   
-
-1   0   1   1   1   
-
-0   1   0   1   0   
-
-0   1   1   0   1   
-
-1   1   0   1   0   
-
-
-
-
-
-
