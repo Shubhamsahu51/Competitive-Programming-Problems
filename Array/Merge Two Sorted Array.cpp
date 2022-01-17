e25d66872eadd82f577c95d8c151c7efd570153d
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2022-01-17 16:40:00 +0530

    Update and rename Merge Two Sorted Array Without Extra Space to Merge Two Sorted Array.cpp

--- Array/Merge Two Sorted Array.cpp
+++ Array/Merge Two Sorted Array.cpp
@@ -60,3 +60,26 @@ public:
 	    sort (arr2,arr2+m);
 	}
 };
+
+ii) with extra space-t(n)=o(m+n)
+	class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        vector<int>ans(m+n,0);
+        int i=0,j=0,k=0;
+        while (i<m and j<n) {
+            if (nums1[i]<nums2[j] )
+                ans[k++]=nums1[i++];
+            else
+                ans[k++]=nums2[j++];
+        }
+            for (;i<m;i++)
+                ans[k++]=nums1[i];
+            for (;j<n;j++)
+                ans[k++]=nums2[j];
+        
+         nums1.swap(ans);
+        
+    }
+};
