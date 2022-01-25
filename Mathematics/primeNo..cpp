f1e90028d67050acdda456bedc62186eb9d71573
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2022-01-25 17:41:36 +0530

    Update primeNo..cpp

--- Mathematics/primeNo..cpp
+++ Mathematics/primeNo..cpp
@@ -7,3 +7,17 @@
         }
         return 1;
     }
+-------------------------------optimise---------------------------
+       int isPrime(int N){
+        if (N==1)
+            return 0;
+        if (N==2 or N==3)
+            return 1;
+        if (N%2==0 or N%3==0)
+            return 0;
+        for (int i=5;i*i<=N;i=i+6) {
+            if (N%i==0 or N%(i+2)==0)
+                return 0;
+        }
+        return 1;
+    }
