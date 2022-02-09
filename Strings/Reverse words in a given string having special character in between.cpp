5c7faa5b9abb4266d56db2feaa74c5674ed13097
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2022-02-09 13:46:15 +0530

    Create Reverse words in a given string having special character in between.cpp

---
+++ Strings/Reverse words in a given string having special character in between.cpp
@@ -0,0 +1,22 @@
+string reverseWords(string S) 
+    { 
+      string ans="";
+      vector<string>temp;
+      for (int i=0;i<S.size();i++) {
+          if (S[i]=='.') {
+              temp.push_back(ans);
+              ans="";
+          }
+          else
+            ans+=S[i];
+      }
+      temp.push_back(ans);
+      string res="";
+      int i=0;
+      for (int i=temp.size()-1;i>0;i--) {
+          res+=temp[i];
+          res+='.';
+      }
+      res+=temp[0];
+      return res;
+    } 
