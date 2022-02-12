94ee6b2f9583f2892d6b6ba017ccc674c15a097a
Shubhamsahu51 <shubhamdineshsahu@gmail.com>
2022-02-13 01:02:29 +0530

    Update Lexicigraphic Rank of string.cpp

--- Strings/Lexicigraphic Rank of string.cpp
+++ Strings/Lexicigraphic Rank of string.cpp
@@ -15,3 +15,31 @@ int main()
     
     return 0;
 }
+
+ii)T(n)=O(n)
+    
+       int fact(int n) {
+        if (n==0) return 1;
+        return n*fact(n-1);
+    }
+    int rank(string S){
+        int n=S.length();
+        int mul=fact(n);
+        int res=1;
+        int count[256]={0};
+        for (int i=0;i<n;i++) {
+            if (count[S[i]]==0) count[S[i]]++;
+            else return 0;                       //corner case
+        }
+        for (int i=1;i<256;i++) {
+            count[i]+=count[i-1];
+        } 
+        for (int i=0;i<n-1;i++) {
+            mul=mul/(n-i);
+            res=res+count[S[i]-1]*mul;
+            for (int j=S[i];j<256;j++)
+                count[j]--;
+        }
+        return res;
+    }
+};
